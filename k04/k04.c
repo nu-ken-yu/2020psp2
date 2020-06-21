def8edab20bea2fba408e0eadfeda01a82d319db
nu-ken-yu <koume0924o@outlook.jp>
2020-06-21 15:13:36 +0900

    課題４　提出

--- k04/k04.c
+++ k04/k04.c
@@ -4,40 +4,85 @@
 #include <math.h>
 
 typedef struct heights{
-    int gender;
+    double id;
+    char gender[10];
     double heights;
 } man_data;
 
 int main(void)
 {
+    int i;
+    int tbl_gender;
     int n = 0;
-    char fname[FILENAME_MAX];
-    char buf[256];
+    char fname_height[FILENAME_MAX];
+    char fname_ID[FILENAME_MAX];
+    char buf_height[256];
+    char buf_ID[254];
+    double input_id;
     man_data man[100];
-    FILE* fp;
+    FILE* fp_height, * fp_ID;
 
-    printf("input the filename of sample:");
-    fgets(fname,sizeof(fname),stdin);
-    fname[strlen(fname)-1] = '\0';
-    printf("the filename of sample: %s\n",fname);
+    printf("input the filename of sample height:");
+    fgets(fname_height,sizeof(fname_height),stdin);
+    fname_height[strlen(fname_height)-1] = '\0';
+    
+    printf("input the filename of sample ID:");
+    fgets(fname_ID,sizeof(fname_ID),stdin);
+    fname_ID[strlen(fname_ID)-1] = '\0';
+    printf("Which ID's data do you want? : ");
+    scanf("%lf", &input_id);
 
-    fp = fopen(fname,"r");
-    if(fp==NULL){
+    fp_height = fopen(fname_height,"r");
+    if(fp_height==NULL){
         fputs("File open error\n",stderr);
         exit(EXIT_FAILURE);
     }
-    fgets(buf, sizeof(buf), fp);
-    while(fgets(buf, sizeof(buf), fp) != NULL){
-        sscanf(buf, "%d, %lf", &man[n].gender, &man[n].heights);
-        printf("%d, %lf\n", man[n].gender, man[n].heights);
+
+    fp_ID = fopen(fname_ID, "r");
+    if(fp_ID==NULL){
+        fputs("File open error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    fgets(buf_height, sizeof(buf_height), fp_height);
+    while(fgets(buf_height, sizeof(buf_height), fp_height) != NULL && fgets(buf_ID, sizeof(buf_ID), fp_ID) != NULL){
+        sscanf(buf_ID, "%lf", &man[n].id);
+        sscanf(buf_height, "%d, %lf", &tbl_gender, &man[n].heights);
+        if(tbl_gender == 1){
+            strcpy(man[n].gender, "Male");
+        }
+        else{
+            strcpy(man[n].gender, "Female");
+        }
         n++;
     }
 
-    if(fclose(fp) == EOF){
+    printf("\n");
+    for(i = 0; i < n; i++){
+        if(input_id == man[i].id){
+            printf("ID : %.0lf\n", man[i].id);
+            printf("Gender : %s\n", man[i].gender);
+            printf("Heights : %.2lf\n", man[i].heights);
+            break;
+        }
+        else{
+            continue;
+        }
+    }
+
+    if(i == n){
+        printf("NO data\n");
+    }
+
+    if(fclose(fp_height) == EOF){
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
     }
 
+    if(fclose(fp_ID) == EOF){
+        fputs("file close error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 
